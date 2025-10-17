a37ede237f202f9bd87c17160eb1b4d2c99ae5ac
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-17 16:12:27 +0530

    chenged in this app

---
+++ lib/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column(
+      children: [
+        Text("nagma")
+      ],
+    ));
+  }
+}
