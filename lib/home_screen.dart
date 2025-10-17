e61cd2537295ed4a5470ae30f8360bcd8a937f74
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-17 16:06:36 +0530

    4 chenged in this app

---
+++ lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column(
+      children: [
+        Text("dfghjkl")
+      ],
+    ));
+  }
+}
