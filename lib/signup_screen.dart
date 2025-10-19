3a5051d7f1844cb2535f6360d63c3cc723d8cde5
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-19 12:37:19 +0530

    1 chenged in this app

---
+++ lib/signup_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column(
+      children: [
+        Text("ghjkl")
+      ],
+    ));
+  }
+}
